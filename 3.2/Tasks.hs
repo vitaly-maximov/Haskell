794f644b80a95b1282e5588de52d19a38225abe6
Vitalii Maksimov <g.vitaly.maximov@gmail.com>
2017-02-19 00:50:56 +0300

    no message

--- 3.2/Tasks.hs
+++ 3.2/Tasks.hs
@@ -16,3 +16,45 @@ filterDisj p1 p2 (x : xs)
 		where
 			rest = filterDisj p1 p2 xs
 -}
+
+{- 
+qsort [1,3,2,5]
+[1,2,3,5]
+-}
+
+qsort :: Ord a => [a] -> [a]
+qsort [] = []
+qsort xs' @ (x : xs) = (qsort l) ++ e ++ (qsort g)
+	where 
+		l = filter (\a -> a < x) xs'
+		e = filter (\a -> a == x) xs'
+		g = filter (\a -> a > x) xs'
+
+-- squares'n'cubes :: Num a => [a] -> [a]
+-- squares'n'cubes = concatMap (\x -> [x ^ 2, x ^ 3])
+
+
+{-
+perms [1,2,3]
+[[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+-}
+
+{-
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms (x : xs) = iterate [] x xs  
+	where
+		iterate [] x [] = [[x]]
+		iterate start x [] = (map (\p -> (x : p)) (perms start))
+		iterate start x end = (map (\p -> (x : p)) (perms (start ++ end))) ++ (iterate (start ++ [x]) (head end) (tail end))
+-}
+
+perms :: [a] -> [[a]]
+perms [] = [[]]
+perms xs = concatMap (\ (x, rs) -> map (x :) $ perms rs) $ iterate [] xs
+	where 
+		iterate _ [] = []
+		iterate hs (x : ts) = (x, hs ++ ts) : iterate (x : hs) ts
+
+delAllUpper :: String -> String
+delAllUpper = unwords . filter (any isLower) . words
