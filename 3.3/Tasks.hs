e17b2ab0cc0b0b9c308837258b8c7fb4b15887db
vitaly-maximov <g.vitaly.maximov@gmail.com>
2017-02-21 11:37:46 +0300

    3.3

--- 3.3/Tasks.hs
+++ 3.3/Tasks.hs
@@ -8,6 +8,11 @@ take 10 $ fibStream
 fibStream :: [Integer]
 fibStream = 0 : 1 : (zipWith (+) fibStream $ drop 1 fibStream)
 
+{-
+succ $ Odd (-100000000000003)
+Odd (-100000000000001)
+-}
+
 data Odd = Odd Integer 
   deriving (Eq, Show)
 
@@ -22,3 +27,21 @@ instance Enum Odd where
 	enumFromThen (Odd x) (Odd y) = map Odd [x, y ..]
 	enumFromTo (Odd x) (Odd z) = map Odd [x, x + 2 .. z]
 	enumFromThenTo (Odd x) (Odd y) (Odd z) = map Odd [x, y .. z]
+
+{-
+change 7
+[[2,2,3],[2,3,2],[3,2,2],[7]]
+-}
+
+coins :: (Ord a, Num a) => [a]
+coins = [2, 3, 7]
+
+change :: (Ord a, Num a) => a -> [[a]]
+change 0 = [[]]
+change n = [coin : set | coin <- coins, coin <= n, set <- change $ n - coin]
+
+{-
+change 0 = [[]]
+change n = concat [map (coin :) $ change $ n - coin | coin <- coins, coin <= n]
+-}
+
