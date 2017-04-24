c39a6acd8e52203aaa9b739390332a3b667f5d0e
Vitalii Maksimov <g.vitaly.maximov@gmail.com>
2017-04-25 00:33:04 +0300

    no message

--- 5.7/T2.hs
+++ 5.7/T2.hs
@@ -4,6 +4,12 @@ import Data.Monoid
 import Control.Monad.Writer
 import Control.Monad.State
 
+{-
+GHCi> runState (writerToState $ tell "world") "hello,"
+((),"hello,world")
+GHCi> runState (writerToState $ tell "world") mempty
+((),"world")
+-}
 
 writerToState :: Monoid w => Writer w a -> State w a
 writerToState m = do
